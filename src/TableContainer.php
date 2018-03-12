081911f0a86e373a7334925d9c19a6a4f244acae
Peter Gorgone <peter@peltronic.com>
2018-03-12 11:08:24 -0700

    lib impl

--- src/TableContainer.php
+++ src/TableContainer.php
@@ -15,25 +15,48 @@ class TableContainer
 
     // %TODO: NOT sure this will work
     //   how will you do: $r->ownername = $r->owner->username;
-    public function addColumn($_data, $_title, $_name=null)
+    //   $_setter is a closure
+    public function addColumn(String $_data, String $_title, Function? $_setter=null:String, String? $_name=null)
     {
         $this->_columns[] = [
                                 'data'   => $_data,
                                 'title'  => $_title,
+                                'setter'  => $_setter, // closure
                                 'name'   => !empty($_name) ? $_name : $_data;
                             ];
     }
 
-    public function addLinkColumn($_data, $_title, $routeName, $routeArgs=null, $_name=null)
+    public function columnConfig()
     {
-        //$r->guid_link = link_to_route('site.widgets.show',$r->renderField('guid'),$r->guid)->toHtml();
-                        [ "data"=>"slug_link", "title"=>"Slug", "name"=>"slug_link" ],
-        $this->_columns[] = [
-                                'data'   => link_to_route($routeName,$_data,
-                                'title'  => $_title,
-                                'name'   => !empty($_name) ? $_name : $_data;
-                            ];
-        // %TODO : check for implements renderField() and if it does use it here??
+        $config = [ 'columns'=>[] ];
+        foreach ($this->_columns as $c) {
+            $config['columns'][] = [ 'data'=>$c->data, 'title'=>$c->title, 'name'=>$c->name ];
+        }
+        return $config;
+    }
+
+    public function renderColumnVals($records)
+    {
+        $columns = $this->_columns;
+        $records->each(function($r) use($options) use($columns) { // Render html for each row's inline form /*
+            // %TODO: patterns: linkify(), render()
+            //$r->pole_id_number_link = link_to_route('site.poles.show',$r->renderField('pole_id_number'),$r->pole_id_number)->toHtml();
+            //$r->crossarm_html = PcrossarmEnum::render($r->crossarm);
+            //$r->pole_class_html = PclassEnum::render($r->pole_class);
+            //$r->pole_condition_html = PconditionEnum::render($r->pole_condition);
+            /*
+            $r->ownername = $r->owner->username; // %FIXME: how to use ->renderField() here??
+            $r->guid_link = link_to_route('site.widgets.show',$r->renderField('guid'),$r->guid)->toHtml();
+            $r->slug_link = link_to_route('site.widgets.show',$r->renderField('slug'),$r->slug)->toHtml();
+             */
+            foreach ($columns as $c) {
+                $cname = $c->name;
+                if ( !is_null($c->setter) && is_callable($c->setter) ) {
+                    $r->{$cname} = $c->setter($r); // use closure
+                } // ...otherwise use 'raw' value based on cname (default, no action needed here)
+            }
+        });
+        return $records;
     }
 
 }
