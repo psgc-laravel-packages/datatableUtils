bc439de18f9394fe54e48d7769c9192f95539b6e
Peter Gorgone <peter@peltronic.com>
2018-03-12 18:44:24 -0700

    updated

--- src/TableContainer.php
+++ src/TableContainer.php
@@ -1,29 +1,30 @@
 <?php
 namespace PsgcLaravelPackages\DatatableUtils;
 
+use Closure;
+use stdClass;
+
 class TableContainer
 {
 
     protected $_columns;
-    protected $_resourceName;
+    public $tablename;
 
-    public function __construct($resourceName)
+    public function __construct($tablename)
     {
         $this->_columns = [];
-        $this->_resourceName = $resourceName;
+        $this->tablename = $tablename;
     }
 
-    // %TODO: NOT sure this will work
-    //   how will you do: $r->ownername = $r->owner->username;
-    //   $_setter is a closure
-    public function addColumn(String $_data, String $_title, Function? $_setter=null:String, String? $_name=null)
+    // $_setter is a closure
+    public function addColumn(String $_data, String $_title, Closure $_setter=null, String $_name=null)
     {
-        $this->_columns[] = [
-                                'data'   => $_data,
-                                'title'  => $_title,
-                                'setter'  => $_setter, // closure
-                                'name'   => !empty($_name) ? $_name : $_data;
-                            ];
+        $c = new stdClass();
+        $c->data   = $_data;
+        $c->title  = $_title;
+        $c->setter  = $_setter; // closure
+        $c->name   = !empty($_name) ? $_name : $_data;
+        $this->_columns[] = $c;
     }
 
     public function columnConfig()
@@ -35,10 +36,25 @@ class TableContainer
         return $config;
     }
 
-    public function renderColumnVals($records)
+    // Set rendering for special fields such as links, FKs, etc
+    //   ~ if not listed here will just default to 'pass-through' of raw column/field name's value
+    public function renderColumnVals(&$records)
     {
         $columns = $this->_columns;
-        $records->each(function($r) use($options) use($columns) { // Render html for each row's inline form /*
+        $records->each(function($r) use($columns) { // Render html for each row's inline form /*
+            foreach ($columns as $c) {
+                $cname = $c->name;
+                if ( !is_null($c->setter) && is_callable($c->setter) ) {
+                    $r->{$cname} = ($c->setter)($r); // use closure
+                } // ...otherwise use 'raw' value based on cname (default, no action needed here)
+            }
+        });
+        return $records;
+    }
+
+}
+
+
             // %TODO: patterns: linkify(), render()
             //$r->pole_id_number_link = link_to_route('site.poles.show',$r->renderField('pole_id_number'),$r->pole_id_number)->toHtml();
             //$r->crossarm_html = PcrossarmEnum::render($r->crossarm);
@@ -49,14 +65,3 @@ class TableContainer
             $r->guid_link = link_to_route('site.widgets.show',$r->renderField('guid'),$r->guid)->toHtml();
             $r->slug_link = link_to_route('site.widgets.show',$r->renderField('slug'),$r->slug)->toHtml();
              */
-            foreach ($columns as $c) {
-                $cname = $c->name;
-                if ( !is_null($c->setter) && is_callable($c->setter) ) {
-                    $r->{$cname} = $c->setter($r); // use closure
-                } // ...otherwise use 'raw' value based on cname (default, no action needed here)
-            }
-        });
-        return $records;
-    }
-
-}
